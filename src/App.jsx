4ce7167aba4cd8010b1fd3a6d3234d87dd3068f2
Fred56733 <fdebiase2020@fau.edu>
2025-01-30 17:08:47 -0500

    enhanced flashcard styling and added images for questions

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import './App.css';
 import { useState } from 'react';
 
 const flashcards = [
-  { question: 'What is the name of the front of a boat?', answer: 'Bow', Image: ''},
-  { question: 'What is the name of the back of the boat?', answer: 'Stern', Image: ''},
-  { question: 'What is the name of the left side of a boat?', answer: 'Port', Image: ''},
-  { question: 'What is the name of the right side of a boat?', answer: 'Starboard', Image: ''},
-  { question: 'What is the name of the location where the steering wheel of a boat is?', answer: 'Helm', Image: ''},
-  { question: 'What is the name of where you tie a boat to?', answer: 'Cleat', Image: ''},
-  { question: 'What is the name of an item used to keep the boat from rubbing on the dock?', answer: 'Fender', Image: ''},
-  { question: 'What is the name of the item used to keep the boat stationary while on the water?', answer: 'Anchor', Image: ''},
-  { question: 'What is the name of the wave a boat produces?', answer: 'Wake', Image: ''},
-  { question: 'What is the width of a boat called?', answer: 'Beam', Image: ''},
+  { question: 'What is the name of the front of a boat?', answer: 'Bow', Image: 'src/assets/Bow.jpg', difficulty: 'easy'},
+  { question: 'What is the name of the back of the boat?', answer: 'Stern', Image: 'src/assets/Stern.jpg', difficulty: 'easy'},
+  { question: 'What is the name of the left side of a boat?', answer: 'Port', Image: 'src/assets/Port.jpg', difficulty: 'easy'},
+  { question: 'What is the name of the right side of a boat?', answer: 'Starboard', Image: 'src/assets/Starboard.jpg', difficulty: 'easy'},
+  { question: 'What is the name of where you tie a boat to?', answer: 'Cleat', Image: 'src/assets/Cleat.jpg', difficulty: 'medium'},
+  { question: 'What is the name of an item used to keep the boat from rubbing on the dock?', answer: 'Fender', Image: 'src/assets/Fender.jpg', difficulty: 'medium'},
+  { question: 'What is the name of the item used to keep the boat stationary while on the water?', answer: 'Anchor', Image: 'src/assets/Anchor.jpg', difficulty: 'medium'},
+  { question: 'What is the name of the wave a boat produces?', answer: 'Wake', Image: 'src/assets/Wake.JPG', difficulty: 'medium'},
+  { question: 'What is the name of the location where the steering wheel of a boat is?', answer: 'Helm', Image: 'src/assets/Helm.jpg', difficulty: 'hard'},
+  { question: 'What is the width of a boat called?', answer: 'Beam', Image: 'src/assets/Beam.jpg', difficulty: 'hard'},
 ]
 
 const App = () => {
@@ -40,11 +40,11 @@ const App = () => {
     <div className="App">
       <h1>Boating 101 Flashcard's</h1>
       <p>Total Cards: {flashcards.length}</p>
-      
       <div className="flashcard-container">
-        <div className={`flashcard ${isFlipped ? 'flipped' : ''}`} onClick = {cardClicked}>
+      <div className={`flashcard ${isFlipped ? 'flipped' : ''} ${flashcards[state].difficulty}`} onClick={cardClicked}>
           <div className="flashcard-front">
             <p>{flashcards[state].question}</p>
+            <img src={flashcards[state].Image} alt="Boat" />
           </div>
           <div className="flashcard-back">
             <p>{flashcards[state].answer}</p>
