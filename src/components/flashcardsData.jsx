b2fe6e60ba45109eb87dd3a0d78ccd0186f09f02
Fred56733 <fdebiase2020@fau.edu>
2025-02-10 21:51:15 -0500

    Made flashcard data its own .jsx file

---
+++ src/components/flashcardsData.jsx
@@ -0,0 +1,14 @@
+const flashcards = [
+  { question: 'What is the name of the front of a boat?', answer: 'Bow', Image: 'src/assets/Bow.jpg', difficulty: 'easy'},
+  { question: 'What is the name of the back of the boat?', answer: 'Stern', Image: 'src/assets/Stern.jpg', difficulty: 'easy'},
+  { question: 'What is the name of the left side of a boat?', answer: 'Port', Image: 'src/assets/Port.jpg', difficulty: 'easy'},
+  { question: 'What is the name of the right side of a boat?', answer: 'Starboard', Image: 'src/assets/Starboard.jpg', difficulty: 'easy'},
+  { question: 'What is the name of where you tie a boat to?', answer: 'Cleat', Image: 'src/assets/Cleat.jpg', difficulty: 'medium'},
+  { question: 'What is the name of an item used to keep the boat from rubbing on the dock?', answer: 'Fender', Image: 'src/assets/Fender.jpg', difficulty: 'medium'},
+  { question: 'What is the name of the item used to keep the boat stationary while on the water?', answer: 'Anchor', Image: 'src/assets/Anchor.jpg', difficulty: 'medium'},
+  { question: 'What is the name of the wave a boat produces?', answer: 'Wake', Image: 'src/assets/Wake.JPG', difficulty: 'medium'},
+  { question: 'What is the name of the location where the steering wheel of a boat is?', answer: 'Helm', Image: 'src/assets/Helm.jpg', difficulty: 'hard'},
+  { question: 'What is the width of a boat called?', answer: 'Beam', Image: 'src/assets/Beam.jpg', difficulty: 'hard'},
+]
+
+export default flashcards
